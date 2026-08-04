0d3085a9ec7a79f506858a06aa851a39c38f16be
dhanti-glitch-2 <dhanti1331@gmail.com>
2026-08-04 22:00:53 +0700

    Create desa.ts

---
+++ data/desa.ts
@@ -0,0 +1,49 @@
+export const desa = {
+  nama: "Desa Karanggeneng",
+  kecamatan: "Kecamatan Pitu",
+  kabupaten: "Kabupaten Ngawi",
+  provinsi: "Provinsi Jawa Timur",
+  tagline: "Tiga dusun, satu jati diri.",
+  deskripsiSingkat:
+    "Di antara barisan pohon jati dan hamparan sawah Kecamatan Pitu, warga tiga dusun merawat ladang, menghidupkan dapur rumahan, dan membuka usahanya sendiri.",
+  profil: [
+    "Desa Karanggeneng merupakan salah satu desa yang berada di wilayah Kecamatan Pitu, Kabupaten Ngawi, Provinsi Jawa Timur. Desa ini terdiri dari tiga dusun, yaitu Dusun Kricak, Dusun Balun, dan Dusun Karanggeneng, yang masing-masing memiliki karakteristik dan potensi wilayahnya sendiri, namun tetap bersinergi dalam satu kesatuan pemerintahan desa.",
+    "Sebagai desa yang dikelilingi kekayaan alam, Desa Karanggeneng memiliki beragam potensi sumber daya alam yang menjadi kekuatan utama wilayah ini. Salah satu potensi unggulan adalah kayu jati, yang menjadi salah satu komoditas berharga bagi masyarakat setempat. Selain itu, desa ini juga memiliki hamparan sawah yang luas sebagai penopang sektor pertanian, serta kawasan hutan yang turut menjaga kelestarian lingkungan sekaligus menjadi sumber penghidupan bagi warga sekitar.",
+    "Dengan tiga dusun yang saling melengkapi serta kekayaan alam yang melimpah, Desa Karanggeneng terus berupaya mengembangkan potensi yang dimiliki demi kesejahteraan masyarakat, baik dari sektor pertanian, kehutanan, maupun potensi ekonomi lainnya yang dapat dikembangkan ke depannya.",
+    "Selain kekayaan alam yang menjadi ciri khasnya, geliat ekonomi masyarakat Desa Karanggeneng juga tercermin dari tumbuhnya berbagai usaha rumahan yang dijalankan oleh warga setempat. Beragam Usaha Mikro, Kecil, dan Menengah (UMKM) hadir sebagai bukti nyata semangat dan kreativitas masyarakat dalam menciptakan lapangan usaha sendiri, mulai dari sektor kuliner hingga jasa.",
+  ],
+  dusun: [
+    {
+      nama: "Dusun Kricak",
+      keterangan: "Rumah bagi Kedai Orizqi dan Pawon Bunda Anwil, denyut kuliner rumahan desa.",
+    },
+    {
+      nama: "Dusun Balun",
+      keterangan: "Tempat Keripik Tempe Bu Kinasih diproduksi dan dipasarkan sebagai oleh-oleh khas.",
+    },
+    {
+      nama: "Dusun Karanggeneng",
+      keterangan: "Pusat desa, tempat Lapak Bu Nana dan Laundry Bu Ida melayani warga sehari-hari.",
+    },
+  ],
+  potensi: [
+    {
+      judul: "Kayu Jati",
+      deskripsi:
+        "Hutan jati yang membentang di sepanjang jalan desa menjadi komoditas berharga sekaligus penanda khas lanskap Karanggeneng.",
+      gambar: "/images/kayu-jati.jpg",
+    },
+    {
+      judul: "Sawah & Pertanian",
+      deskripsi:
+        "Hamparan sawah hijau menjadi penopang utama sektor pertanian dan sumber penghidupan sebagian besar warga.",
+      gambar: "/images/sawah.jpg",
+    },
+    {
+      judul: "Hutan & Lingkungan",
+      deskripsi:
+        "Kawasan hutan di sekeliling desa turut menjaga kelestarian lingkungan sekaligus menopang penghidupan warga sekitar.",
+      gambar: "/images/gapura.jpg",
+    },
+  ],
+};
